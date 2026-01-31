00a1650b4acc1c8e5b3fd72439671036c2486d7d
Pranish Poudel <pranishpoudel02@gmail.com>
2026-01-31 12:53:25 +0545

    commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Michroma, Lusitana } from 'next/font/google';
+ 
+export const michroma = Michroma({
+    subsets: ['latin'],
+    weight: '400'
+});
+export const lusitana  = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
